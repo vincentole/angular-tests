221ca0c7242064364c12babd68008ea763f1b847
vincentole <63076449+vincentole@users.noreply.github.com>
2023-10-16 01:34:46 +0200

    reactive declarative vs imperative

---
+++ src/app/pages/index.page copy.ts
@@ -0,0 +1,91 @@
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [NgFor, NgClass, NgIf],
+  template: `
+    <div class="grid grid-cols-5 grid-rows-5 gap-4">
+      <button
+        *ngFor="let isCellActive of grid; index as i"
+        class="rounded-md p-12"
+        [class.bg-red-500]="isCellActive"
+        [class.bg-stone-400]="!isCellActive"
+        (click)="toggle(i)"
+      ></button>
+    </div>
+    <p class="text-4xl mt-8" [ngClass]="{ invisible: !won }">You won!</p>
+    <button class="mr-1 mt-4 border rounded-lg px-4 py-1" (click)="reset()">
+      Reset Game
+    </button>
+    <button
+      class="ml-1 mt-4 border rounded-lg px-4 py-1 bg-stone-300 text-stone-900 font-bold"
+      *ngIf="won"
+      (click)="nextLevel()"
+    >
+      Next Level
+    </button>
+  `,
+})
+export default class HomeComponent implements OnInit {
+  grid: boolean[] = new Array(25).fill(false);
+  levels: boolean[][] = [];
+  current_level = 0;
+  won = false;
+
+  ngOnInit(): void {
+    let level_1 = new Array(25).fill(false);
+    [10, 12, 14].forEach((i) => (level_1[i] = true));
+
+    let level_2 = new Array(25).fill(false);
+    [5, 7, 9].forEach((i) => (level_2[i] = true));
+
+    this.levels = [level_1, level_2];
+
+    this.setGrid(this.levels[this.current_level]);
+  }
+
+  toggle(i: number): void {
+    this.grid[i] = !this.grid[i];
+
+    if (i - 5 > 0) {
+      this.grid[i - 5] = !this.grid[i - 5];
+    }
+
+    if (i % 5 !== 0) {
+      this.grid[i - 1] = !this.grid[i - 1];
+    }
+
+    if (i + 5 < this.grid.length) {
+      this.grid[i + 5] = !this.grid[i + 5];
+    }
+
+    if ((i + 1) % 5 !== 0) {
+      this.grid[i + 1] = !this.grid[i + 1];
+    }
+
+    if (this.grid.every((v) => v === false)) {
+      this.won = true;
+    }
+  }
+
+  reset(): void {
+    this.setGrid(this.levels[this.current_level]);
+    this.won = false;
+  }
+
+  nextLevel(): void {
+    if (this.current_level < this.levels.length - 1) {
+      this.current_level += 1;
+    } else {
+      this.current_level = 0;
+    }
+    this.setGrid(this.levels[this.current_level]);
+    this.won = false;
+  }
+
+  setGrid(grid: boolean[]): void {
+    this.grid = [...grid];
+  }
+}
--- src/app/pages/index.page.ts
+++ src/app/pages/index.page.ts
@@ -1,49 +1,122 @@
+import { NgClass, NgFor, NgIf, AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { Subject, map, merge, of, scan, startWith, withLatestFrom } from 'rxjs';
+
+let level_1 = new Array(25).fill(false);
+[10, 12, 14].forEach((i) => (level_1[i] = true));
+
+let level_2 = new Array(25).fill(false);
+[7, 11, 12, 13, 17].forEach((i) => (level_2[i] = true));
+
+const levels = [level_1, level_2];
+
+function getLevel(i: number) {
+  return [...levels[i]];
+}
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [NgFor, NgClass, NgIf, AsyncPipe],
   template: `
-    <div>
-      <a href="https://analogjs.org/" target="_blank">
-        <img alt="Analog Logo" class="logo analog" src="/analog.svg" />
-      </a>
+    <div class="grid grid-cols-5 grid-rows-5 gap-4">
+      <button
+        *ngFor="let isCellActive of grid$ | async; index as i"
+        class="rounded-md p-12"
+        [class.bg-red-500]="isCellActive"
+        [class.bg-stone-400]="!isCellActive"
+        (click)="toggleTrigger$.next(i)"
+      ></button>
     </div>
-
-    <h2>Analog</h2>
-
-    <h3>The fullstack meta-framework for Angular!</h3>
-
-    <div class="card">
-      <button type="button" (click)="increment()">Count {{ count }}</button>
-    </div>
-
-    <p class="read-the-docs">
-      For guides on how to customize this project, visit the
-      <a href="https://analogjs.org" target="_blank">Analog documentation</a>
+    <p class="text-4xl mt-8" [ngClass]="{ invisible: !(won$ | async) }">
+      You won!
     </p>
+    <button
+      class="mr-1 mt-4 border rounded-lg px-4 py-1"
+      (click)="resetTrigger$.next()"
+    >
+      Reset Game
+    </button>
+    <button
+      class="ml-1 mt-4 border rounded-lg px-4 py-1 bg-stone-300 text-stone-900 font-bold"
+      *ngIf="won$ | async"
+      (click)="nextLevelTrigger$.next()"
+    >
+      Next Level
+    </button>
   `,
-  styles: [
-    `
-      .logo {
-        will-change: filter;
-      }
-      .logo:hover {
-        filter: drop-shadow(0 0 2em #646cffaa);
-      }
-      .logo.angular:hover {
-        filter: drop-shadow(0 0 2em #42b883aa);
-      }
-      .read-the-docs {
-        color: #888;
-      }
-    `,
-  ],
 })
 export default class HomeComponent {
-  count = 0;
+  // Sources
+  resetTrigger$ = new Subject<void>();
+  nextLevelTrigger$ = new Subject<void>();
+  toggleTrigger$ = new Subject<number>();
 
-  increment() {
-    this.count++;
-  }
+  // States
+  currentLevel$ = merge(
+    of({}),
+    this.nextLevelTrigger$.pipe(map(() => ({ delta: 1 })))
+  ).pipe(
+    scan((acc, curr: { delta?: number }) => {
+      if (!curr.delta) return acc;
+
+      if (acc + curr.delta >= levels.length) {
+        return 0;
+      }
+      return acc + curr.delta;
+    }, 0)
+  );
+
+  grid$ = merge(
+    of({}),
+    this.resetTrigger$.pipe(
+      withLatestFrom(this.currentLevel$),
+      map(([, level]) => ({ level }))
+    ),
+    this.currentLevel$.pipe(
+      map((level) => ({
+        level,
+      }))
+    ),
+    this.toggleTrigger$.pipe(map((i) => ({ i })))
+  ).pipe(
+    scan<{ level?: number; i?: number; reset?: boolean }, boolean[]>(
+      (acc, curr) => {
+        if (curr.level !== undefined) {
+          return getLevel(curr.level);
+        }
+
+        if (curr.i) {
+          acc[curr.i] = !acc[curr.i];
+
+          if (curr.i - 5 > 0) {
+            acc[curr.i - 5] = !acc[curr.i - 5];
+          }
+
+          if (curr.i % 5 !== 0) {
+            acc[curr.i - 1] = !acc[curr.i - 1];
+          }
+
+          if (curr.i + 5 < acc.length) {
+            acc[curr.i + 5] = !acc[curr.i + 5];
+          }
+
+          if ((curr.i + 1) % 5 !== 0) {
+            acc[curr.i + 1] = !acc[curr.i + 1];
+          }
+        }
+
+        return acc;
+      },
+      getLevel(0)
+    )
+  );
+
+  won$ = merge(
+    merge(this.resetTrigger$, this.nextLevelTrigger$).pipe(map(() => false)),
+    this.toggleTrigger$.pipe(
+      withLatestFrom(this.grid$),
+      map(([, grid]) => grid.every((v) => v === false))
+    )
+  ).pipe(startWith(false));
 }
